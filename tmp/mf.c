999fceb07ddddad90672debb9d666ad40c74da25
mfindra <miskofindra@gmail.com>
2020-10-30 17:53:21 +0100

    Revork to case

--- tmp/mf.c
+++ tmp/mf.c
@@ -1,54 +1,77 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[]) {  
-    char sign;
-    int white_space[3] = {32,10,9}; // "9", "10" breaks condition - check
+int main(int argc, char *argv[])
+{
+    int currentChar;
+    int whiteSpace[3] = {' ', '\t', '\n'};
     char token[20][20];
     int letter = 0;
     int word = 0;
 
-    sign = getchar();
-      // remove whitespaces from beginnning
-    while (sign == white_space[0] || sign == white_space[1]){
-        sign = getchar();   
-    }   
-    do { 
-        // START state
+    // remove whitespaces from beginnning
+    while (currentChar == whiteSpace[0] || currentChar == whiteSpace[1])
+    {
+        currentChar = getchar();
+    }
 
-        if (sign == 47){
-            sign = getchar();
-                if (sign == 47){
-                    //line comment
-                    while (sign != 10)
-                        sign = getchar();                   
-                } else if (sign == 42){
-                    // block comment
-                    Label: 
-                    while (sign != 42){
-                        sign = getchar();
-                    }
-                    sign = getchar();
-                    if (sign != 47) goto Label;  // !!! needs further checking to prevent segfault                  
-                }           
-        } else {
-            if (sign != white_space[0]){   // chained condition breaks parsing ((sign != white_space[0]) || (sign != white_space[1]) || (sign != white_space[2]) )
-            token[word][letter]=sign;
-            letter++;
-            } else {           
-                word++;   
-                letter = 0;         
-            }       
-            sign = getchar();  
+    // loop through every char in file
+    do
+    {
+        currentChar = getchar();
+
+        switch (currentChar)
+        {
+        case EOF:
+            break;
+        case ' ':
+        case '\t':
+        case '\n':
+            break;
+        // COMMENT
+        case '/':
+            currentChar = getchar();
+            // line comment
+            if (currentChar == '/')
+            {
+                do
+                {
+                    currentChar = getchar();
+                } while (currentChar != '\n');
+            }
+            // block comment
+            else if (currentChar == '*')
+            {
+                int prevChar;
+                do
+                {
+                    prevChar = currentChar;
+                    currentChar = getchar();
+                    //printf("%d, %d\n", prevChar, currentChar);
+
+                } while ((currentChar != '/') || (prevChar != '*'));
+            }
+            // TODO: variable starting with /
+            break;
+        // TOKENS
+        default:
+            do
+            {
+                token[word][letter] = currentChar;
+                letter++;
+                currentChar = getchar();
+            } while (currentChar != ' ' && currentChar != '\t');
+            word++;
+            letter = 0;
+            break;
         }
 
-    } while (sign != EOF);
-
+    } while (currentChar != EOF);
 
     printf("%s\n", token[0]);
     printf("%s\n", token[1]);
-    
+    printf("%s\n", token[2]);
+    printf("%s\n", token[3]);
 
     return 0;
 }
