1b87da9b0ecece05b3a50b700167f826e129c479
mfindra <miskofindra@gmail.com>
2020-10-31 12:23:02 +0100

    case rework???

--- tmp/mf.c
+++ tmp/mf.c
@@ -1,23 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum AS
+{
+    AS_WhiteSign,
+    AS_BlockComm,
+    AS_LineComm,
+    AS_Comm,
+    AS_Token
+};
+
 int main(int argc, char *argv[])
 {
-    int currentChar;
-    int whiteSpace[3] = {' ', '\t', '\n'};
+    int currentChar, lastChar;
     char token[20][20];
     int letter = 0;
     int word = 0;
-
-    // remove whitespaces from beginnning
-    while (currentChar == whiteSpace[0] || currentChar == whiteSpace[1])
-    {
-        currentChar = getchar();
-    }
+    int AutomatState;
 
     // loop through every char in file
-    do
+    while ((currentChar = getchar()) != EOF)
     {
+        if ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') &&
+            AutomatState != AS_Comm && AutomatState != AS_LineComm && AutomatState != AS_BlockComm)
+        {
+            AutomatState = AS_WhiteSign;
+        }
+        else if (currentChar == '/')
+        {
+            AutomatState = AS_Comm;
+        }
+        else if ((currentChar >= 'a' && currentChar <= 'z') ||
+                 (currentChar >= 'A' && currentChar <= 'Z') ||
+                 (currentChar >= '0' && currentChar <= '9') &&
+                     AutomatState != AS_Comm &&
+                     AutomatState != AS_LineComm &&
+                     AutomatState != AS_BlockComm)
+        {
+            AutomatState = AS_Token;
+            if ((lastChar == ' ' || lastChar == '\t' || lastChar == '\n') && letter != 0)
+            {
+                word++;
+                letter = 0;
+            }
+        }
+
+        switch (AutomatState)
+        {
+        case AS_WhiteSign:
+
+            break;
+
+        case AS_Comm:
+            printf("ahoj\n");
+            if (currentChar == '/' && lastChar == '/')
+                AutomatState = AS_LineComm;
+            break;
+
+        case AS_LineComm:
+            printf("ahojda");
+            break;
+
+        case AS_BlockComm:
+            break;
+
+        case AS_Token:
+            token[word][letter] = currentChar;
+            letter++;
+            break;
+
+        deafult:
+            break;
+        }
+        lastChar = currentChar;
+    }
+
+    printf("%s\n", token[0]);
+    printf("%s\n", token[1]);
+    printf("%s\n", token[2]);
+    printf("%s\n", token[3]);
+
+    return 0;
+}
+/*
+
         currentChar = getchar();
 
         switch (currentChar)
@@ -74,4 +140,4 @@ int main(int argc, char *argv[])
     printf("%s\n", token[3]);
 
     return 0;
-}
+}*/
