6c14977f04fccab7c3e35d7ea5f829ac5e504125
mfindra <miskofindra@gmail.com>
2020-11-01 16:16:11 +0100

    Added cases for comments and testing

--- tmp/mf.c
+++ tmp/mf.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum AS
+typedef enum
 {
     AS_WhiteSign,
     AS_BlockComm,
     AS_LineComm,
     AS_Comm,
-    AS_Token
-};
+    AS_Token,
+    AS_Empty
+} AS;
+
+//check if is int "a" is letter in ASCII
+int isLetter(int a)
+{
+    if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+    {
+        return 0;
+    }
+    return -1;
+}
+
+//check if is int "a" is number in ASCII
+int isNumber(int a)
+{
+    if (a >= '0' && a <= '9')
+    {
+        return 0;
+    }
+    return -1;
+}
+
+//check if AutomateState is Comment
+int CommentActive(AS AutomatState)
+{
+    if (AutomatState == AS_Comm || AutomatState == AS_LineComm || AutomatState == AS_BlockComm)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,33 +47,22 @@ int main(int argc, char *argv[])
     char token[20][20];
     int letter = 0;
     int word = 0;
-    int AutomatState;
+    AS AutomatState = AS_Empty;
 
     // loop through every char in file
     while ((currentChar = getchar()) != EOF)
     {
-        if ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') &&
-            AutomatState != AS_Comm && AutomatState != AS_LineComm && AutomatState != AS_BlockComm)
+        if ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && !CommentActive(AutomatState))
         {
             AutomatState = AS_WhiteSign;
         }
-        else if (currentChar == '/')
+        else if (currentChar == '/' && !CommentActive(AutomatState))
         {
             AutomatState = AS_Comm;
         }
-        else if ((currentChar >= 'a' && currentChar <= 'z') ||
-                 (currentChar >= 'A' && currentChar <= 'Z') ||
-                 (currentChar >= '0' && currentChar <= '9') &&
-                     AutomatState != AS_Comm &&
-                     AutomatState != AS_LineComm &&
-                     AutomatState != AS_BlockComm)
+        else if ((isLetter(currentChar) || isNumber(currentChar)) && !CommentActive(AutomatState))
         {
             AutomatState = AS_Token;
-            if ((lastChar == ' ' || lastChar == '\t' || lastChar == '\n') && letter != 0)
-            {
-                word++;
-                letter = 0;
-            }
         }
 
         switch (AutomatState)
@@ -52,19 +72,28 @@ int main(int argc, char *argv[])
             break;
 
         case AS_Comm:
-            printf("ahoj\n");
             if (currentChar == '/' && lastChar == '/')
                 AutomatState = AS_LineComm;
+            else if (currentChar == '*' && lastChar == '/')
+                AutomatState = AS_BlockComm;
             break;
 
         case AS_LineComm:
-            printf("ahojda");
+            if (currentChar == '\n')
+                AutomatState = AS_Empty;
             break;
 
         case AS_BlockComm:
+            if (currentChar == '/' && lastChar == '*')
+                AutomatState = AS_Empty;
             break;
 
         case AS_Token:
+            if ((lastChar == ' ' || lastChar == '\t' || lastChar == '\n') && letter != 0)
+            {
+                word++;
+                letter = 0;
+            }
             token[word][letter] = currentChar;
             letter++;
             break;
@@ -75,69 +104,11 @@ int main(int argc, char *argv[])
         lastChar = currentChar;
     }
 
-    printf("%s\n", token[0]);
-    printf("%s\n", token[1]);
-    printf("%s\n", token[2]);
-    printf("%s\n", token[3]);
+    // just to check
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", token[i]);
+    }
 
     return 0;
 }
-/*
-
-        currentChar = getchar();
-
-        switch (currentChar)
-        {
-        case EOF:
-            break;
-        case ' ':
-        case '\t':
-        case '\n':
-            break;
-        // COMMENT
-        case '/':
-            currentChar = getchar();
-            // line comment
-            if (currentChar == '/')
-            {
-                do
-                {
-                    currentChar = getchar();
-                } while (currentChar != '\n');
-            }
-            // block comment
-            else if (currentChar == '*')
-            {
-                int prevChar;
-                do
-                {
-                    prevChar = currentChar;
-                    currentChar = getchar();
-                    //printf("%d, %d\n", prevChar, currentChar);
-
-                } while ((currentChar != '/') || (prevChar != '*'));
-            }
-            // TODO: variable starting with /
-            break;
-        // TOKENS
-        default:
-            do
-            {
-                token[word][letter] = currentChar;
-                letter++;
-                currentChar = getchar();
-            } while (currentChar != ' ' && currentChar != '\t');
-            word++;
-            letter = 0;
-            break;
-        }
-
-    } while (currentChar != EOF);
-
-    printf("%s\n", token[0]);
-    printf("%s\n", token[1]);
-    printf("%s\n", token[2]);
-    printf("%s\n", token[3]);
-
-    return 0;
-}*/
