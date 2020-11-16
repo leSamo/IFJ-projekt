7862cf9a4a90397ffc279a16b9f4d22920975b56
leSamo <soleksak@redhat.com>
2020-11-16 18:30:47 +0100

    Repo cleanup

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CFLAGS = -pedantic -Wall -Wextra -std=c11 # -Werror
 CC = gcc
 
 all:
-	$(CC) leParser.c -o leParser.o
+	$(CC) src/leParser.c -o leParser.o
 
 clean:
 	rm *.o
--- src/leParser.h
+++ src/leParser.h
@@ -6,10 +6,16 @@
  * Description: Syntactic analysis using recursive descent
  * ================================= */
 
+/* Returns new non-newline token, but requires at least one newline token prior */
 Token getToken_NL_required();
 
+/* Returns new non-newline token, ignores newline tokens prior */
 Token getToken_NL_optional();
 
+/* Returns true if supplied tokenType could be first token of expression */
+bool isExpFirst(tokenType type);
+
+/* NON-TERMINALS */
 bool NT_Prog();
 
 bool NT_Prolog();
